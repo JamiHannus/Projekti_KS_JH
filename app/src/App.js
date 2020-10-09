6b2d0e4396eb60e5f8313cf9b114f75a32c9a209
Kalle Soukka <t9soka00@students.oamk.fi>
2020-10-09 11:20:54 +0300

    some modifications to stations

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import LogIn from './components/LogIn';
 import InfoCardList from './components/InfoCardList'
 import Stations from './components/Stations';
+import axios from 'axios'
 
 class App extends Component {
- 
+    constructor(props)
+    {
+      super(props);
+          this.state = {
+          items: [],
+      };
+    }
+    componentDidMount() {
+        axios.get('http://localhost:4000/items')
+        .then(res => {
+            this.setState({ items: res.data });
+        });
+      }
+      checkMount(){
+      this.setState({items:!this.state.items,}, function () {
+        this.props.updateItem(this.state);
+      }.bind(this));
+      }
   render () {
+    /*const [testi, setTest] = useState({items})
+    {testi.map((testi) => {
+      return (
+      console.log(testi = {testi}) 
+      )}) 
+  }*/
     return(
       <div>
         <div>
+          <ul>
+            {this.state.items.map(item => (
+              <li key={item.station_data}>{item}</li>
+            ))}
+          </ul>
           {/* <Stations /> */}
           <div>
             <InfoCardList />
@@ -31,6 +60,7 @@ class App extends Component {
     );
   }
 }
+
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyCMqc8MsAIPHk83Y_ED7jQDCR0179SMROc",
--- app/src/components/Stations.jsx
+++ app/src/components/Stations.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios';
 
 class Stations extends React.Component {
-        state = {
+      /*  state = {
             items: []
         };
         
