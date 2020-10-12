eed195bcf95620a28d9d52c7a5c3881548ca115f
Jami Hannus <t9haja00@students.oamk.fi>
2020-10-12 12:16:36 +0300

    Axios

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,39 @@
 import React, { Component, useState } from "react";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MapContainer from "./components/MapContainer";
 import LogIn from "./components/LogIn";
 import InfoCardList from "./components/InfoCardList";
-import Users from "./components/Users";
+
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading :true,
+    };
+  }
+  
+ async componentDidMount() {
+   const response = await axios.get("http://localhost:4000/items");
+    const json = response;
+    this.setState({data: json});
+    console.log(this.state.data);
+  }  
+  
+
+ 
   render() {
     return (
       <div>
         <div>
           <div>
-            <Users />
           </div>
           <div>
-            <InfoCardList />
+            <InfoCardList dataFromParent={this.state.data}/>
           </div>
           <div>
             <LogIn />
--- app/src/components/InfoCardList.jsx
+++ app/src/components/InfoCardList.jsx
@@ -1,38 +1,62 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import MainInfoCard from "./MainInfoCard";
+
+// export default function InfoCardList() {
+//   const SAMPLE_CARDS = [
+//     {
+//       id: 1,
+//       type: 2,
+//       desc: "hello there 1",
+//     },
+
+//     {
+//       id: 2,
+//       type: 2,
+//       desc: "hello there 2",
+//     },
+
+//     {
+//       id: 3,
+//       type: 2,
+//       desc: "hello there 3",
+//     },
+
+//     {
+//       id: 4,
+//       type: 2,
+//       desc: "hello there 4",
+//     },
+//   ];
+//    const [testcards, setTest] = useState(SAMPLE_CARDS);
+//   //const InfoCardList = ({ test = []}) => { 
+  
+//   return (
+//     // <div>
+//     //   {testcards.map((testcard) => {
+//     //     return <MainInfoCard testcard={testcard} key={testcard.id} />;
+//     //   })}
+//     // </div>
+//     <div>
+//        {testcards.map( testcards =>( testcards.station_data))}
+//     </div>
+
+//   );
+// }
+
+import React from "react";
 import MainInfoCard from "./MainInfoCard";
-
-export default function InfoCardList() {
-  const SAMPLE_CARDS = [
-    {
-      id: 1,
-      type: 2,
-      desc: "hello there 1",
-    },
-
-    {
-      id: 2,
-      type: 2,
-      desc: "hello there 2",
-    },
-
-    {
-      id: 3,
-      type: 2,
-      desc: "hello there 3",
-    },
-
-    {
-      id: 4,
-      type: 2,
-      desc: "hello there 4",
-    },
-  ];
-  const [testcards, setTest] = useState(SAMPLE_CARDS);
-  return (
-    <div>
-      {testcards.map((testcard) => {
-        return <MainInfoCard testcard={testcard} key={testcard.id} />;
-      })}
-    </div>
-  );
-}
+const realData = Array.from(this.props.dataFromParent);
+const InfoCardList = ({ realData = [] }) => {
+ 
+    return(
+      realData.map(stations => (
+        <MainInfoCard
+        key={stations.station_data}
+        ID = {stations.station_data.id}
+        type = {stations.type}
+        desc = {stations.description}
+         />
+      ))
+    );
+ }
+export default InfoCardList;
