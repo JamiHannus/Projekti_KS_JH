8a1458d2309baef1109dc5828ac9795e538efd1f
Jami Hannus <t9haja00@students.oamk.fi>
2020-10-12 12:39:20 +0300

    Moved testi to infocardlist and deleted old one

--- app/src/components/InfoCardList.jsx
+++ app/src/components/InfoCardList.jsx
@@ -1,49 +1,17 @@
-import React, { useState } from "react";
-import MainInfoCard from "./MainInfoCard";
+import React from 'react'
 
-/*export default function InfoCardList() {
-  const SAMPLE_CARDS = [
-    {
-      id: 1,
-      type: 2,
-      desc: "hello there 1",
-    },
+import MainInfoCard from './MainInfoCard'
 
-    {
-      id: 2,
-      type: 2,
-      desc: "hello there 2",
-    },
-
-    {
-      id: 3,
-      type: 2,
-      desc: "hello there 3",
-    },
-
-    {
-      id: 4,
-      type: 2,
-      desc: "hello there 4",
-    },
-  ];
-  const [testcards, setTest] = useState(SAMPLE_CARDS);
-  return (
-    <div>
-      {testcards.map((testcard) => {
-        return <MainInfoCard testcard={testcard} key={testcard.id} />;
-      })}
-    </div>
-  );
-}
-*/
-export default function InfoCardList(props) {
-  return(
-    <div>
-      <div>{ props.Type }</div>
-      <div>{ props.lattitude }</div>
-      <div>{ props.longitude }</div>
-      <div>{ props.description }</div>
-    </div>
-  )
+export default function Testi(props) {
+    return (
+        <div>
+            <div>
+                {
+                    props.items.map(item => (
+                    <MainInfoCard key={item.station_data}{...item} />)
+                    )
+                }
+            </div>
+        </div>
+    )
 }
---
+++ app/src/components/InfoCardListOld.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import MainInfoCard from "./MainInfoCard";
+
+/*export default function InfoCardList() {
+  const SAMPLE_CARDS = [
+    {
+      id: 1,
+      type: 2,
+      desc: "hello there 1",
+    },
+
+    {
+      id: 2,
+      type: 2,
+      desc: "hello there 2",
+    },
+
+    {
+      id: 3,
+      type: 2,
+      desc: "hello there 3",
+    },
+
+    {
+      id: 4,
+      type: 2,
+      desc: "hello there 4",
+    },
+  ];
+  const [testcards, setTest] = useState(SAMPLE_CARDS);
+  return (
+    <div>
+      {testcards.map((testcard) => {
+        return <MainInfoCard testcard={testcard} key={testcard.id} />;
+      })}
+    </div>
+  );
+}
+*/
+export default function InfoCardList(props) {
+  
+  return(
+    <div>
+      <div>{ props.station_data }</div>
+      <div>{ props.Type }</div>
+      <div>{ props.lattitude }</div>
+      <div>{ props.longitude }</div>
+      <div>{ props.description }</div>
+    </div>
+  )
+}
